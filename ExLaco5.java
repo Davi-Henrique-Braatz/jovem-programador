249788770691b4e55a9c2f2e5195403aa26a0ac5
Davi-Henrique-Braatz <davihenriquebraatz1@gmail.com>
2026-03-25 10:59:18 -0300

    Add files via upload

---
+++ ExLaco5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class ExLaco5 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+	
+	
+	
+	
+	
+	for (int i =1 ; i <= 5; i++) {
+	
+	System.out.println("Insira a altura: ");	
+	double altura = input.nextDouble();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
