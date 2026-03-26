1784f67cb129c16cd5c9f839b450c147731ec2e1
Davi-Henrique-Braatz <davihenriquebraatz1@gmail.com>
2026-03-26 08:47:18 -0300

    Add files via upload

---
+++ ExLaco6.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class ExLaco6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner input = new Scanner(System.in);
+	
+   double soma = 1;
+  double altura = 1;
+   
+	
+for (int i = 1; i >= altura; i++) {
+	
+	System.out.println("Insira a altura: ");
+	double altura2 = input.nextDouble();
+	altura++; 
+	
+	
+ double somaAltura = soma += altura;
+
+double media = somaAltura / altura;
+
+
+
+
+
+
+if(altura2 == 0) {
+	
+break;
+
+}
+}
+double somaAltura = soma += altura;
+
+double media = somaAltura / altura;
+System.out.println("A media da altura dessas pessoas é: "+media);
+	
+	
+	
+	
+	
+}	
+	
+	}
+
+	
