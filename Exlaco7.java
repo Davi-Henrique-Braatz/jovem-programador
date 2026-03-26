91fecf978de33e322529422b9a1bcc0a5dc21d0b
Davi-Henrique-Braatz <davihenriquebraatz1@gmail.com>
2026-03-26 11:18:15 -0300

    Add files via upload

---
+++ Exlaco7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exlaco7 {
+public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	
+	String nome = "fim";
+	System.out.println("Insira o nome do aluno: ");	
+	String	nome2 = input.nextLine();
+
+	while(!nome2.equals(nome)){
+	
+		System.out.println("Insira nome do aluno: ");	
+	nome2 = input.nextLine();
+	
+	System.out.println("Insira a nota 1: ");
+ double nota1 = input.nextDouble();
+	
+ 
+ System.out.println("Insira a nota 2: ");
+	double nota2 = input.nextDouble();
+	
+	
+	if(nome2.equals(nome)) {
+		
+		break;
+		
+		
+		
+	}}
+	
+		}
+
+}
