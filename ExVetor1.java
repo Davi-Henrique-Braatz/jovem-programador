c2b87d0cd1a7c3c6afa70c2eeb2290dd3cd240e7
Davi-Henrique-Braatz <davihenriquebraatz1@gmail.com>
2026-04-02 10:47:28 -0300

    Add files via upload

---
+++ ExVetor1.java
@@ -0,0 +1,24 @@
+
+public class ExVetor1 {
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	int numero[] = {18, 98, 45, 65, 89};
+	
+	
+	for(int i = numero.length - 1; i >= 0; i--) {
+		System.out.println( numero[i] + " ");
+		
+			
+		
+	}
+	
+	
+	
+}
+}
---
+++ ExVetor6.java
@@ -0,0 +1,33 @@
+
+public class ExVetor6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int vetor[] = new int [6];
+	
+	vetor[0] = 18;
+	vetor[1] = 16;
+	vetor[2] = 10;
+	vetor[3] = 67;
+	vetor[4] = 23;
+	vetor[5] = 89;
+	
+	for (int i = vetor.length - 1; i >= 0; i--) {
+		
+		System.out.println(vetor[i] + " ");
+		
+	}	
+	
+	
+	
+	
+	}
+
+	
+	
+	
+	
+	
+	
+}
